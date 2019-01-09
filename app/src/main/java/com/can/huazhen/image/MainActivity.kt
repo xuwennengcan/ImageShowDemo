6d3cfbbfefd24a060e1e2b6660f174681176d204
xuwenneng <xuwenneng@huazhen.com>
2019-01-09 11:41:43 +0800

    简单的图片加载时转圈效果

--- app/src/main/java/com/can/huazhen/image/MainActivity.kt
+++ app/src/main/java/com/can/huazhen/image/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : Activity() {
     private var iv: GlideImageView? = null
     private var cpv : CircleProgressView?=null
 
-    private val image = "https://raw.githubusercontent.com/sfsheng0322/GlideImageView/master/resources/cat.jpg"
+    private val image = "https://static.dingtalk.com/media/lADPDgQ9qb7pRqvNC9DND8A_4032_3024.jpg?auth_bizType=IM&auth_bizEntity=%7B%22cid%22%3A%224248001%3A284146280%22%2C%22msgId%22%3A%22769665707059%22%7D&open_id=284146280"
     private val thumb = "https://raw.githubusercontent.com/sfsheng0322/GlideImageView/master/resources/cat_thumbnail.jpg"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +30,7 @@ class MainActivity : Activity() {
 
         iv!!.apply(options).error(R.drawable.drawable_default)
 
-        iv!!.load(image,R.drawable.drawable_default) {
+        iv!!.load(image,0) {
             isComplete, percentage, bytesRead, totalBytes ->
             cpv!!.progress = percentage
             cpv!!.visibility = if(isComplete) View.GONE else View.VISIBLE
